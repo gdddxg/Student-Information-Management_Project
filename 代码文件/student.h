5fb5753175b7e373aa66f7bb713a4c0bb7b5520b
terryblock <146404269+terryblock@users.noreply.github.com>
2024-12-25 15:17:48 +0800

    Update student.h

--- 代码文件/student.h
+++ 代码文件/student.h
@@ -1,78 +1,15 @@
 
 // 显示指定学生信息
-void display_student(Node *student) {
-    if (student == NULL) {
-        printf("学生信息为空。\n");
-        return;
+void display_student() {
     }
-
-    printf("学生信息：\n");
-    printf("学号：%s, 姓名：%s, 性别：%s, 年龄：%d, 专业：%s\n",
-           student->data.xh, student->data.xm, student->data.xb, student->data.nl, student->data.zy);
-}
 
 // 学生登录函数
 Node *login() {
-    char id[20];
-    char name[MAX_NAME_LEN];
-    printf("请输入学号：");
-    scanf("%s", id);
-    printf("请输入姓名：");
-    scanf("%s", name);
-
-    Node *current = head;
-    while (current != NULL) {
-        if (strcmp(current->data.xh, id) == 0 && strcmp(current->data.xm, name) == 0) {
-            printf("登录成功！\n");
-            return current;
-        }
-        current = current->next;
-    }
-    printf("登录失败，请检查学号和姓名。\n");
-    return NULL;
 }
 
 // 修改学生信息 (仅限年龄和专业)
-void modify_student_info(Node *student) {
-    if (student == NULL) {
-        printf("学生信息为空，无法修改。\n");
-        return;
-    }
-
-    printf("请输入新的年龄：");
-    scanf("%d", &student->data.nl);
-    printf("请输入新的专业：");
-    scanf("%s", student->data.zy);
-    printf("学生信息修改成功！\n");
-}
+void modify_student_info() 
 
 // 学生操作菜单
 void student_menu() {
-    Node *student = login();
-    if (student == NULL) {
-        return;
     }
-
-    int choice;
-    while (1) {
-        printf("\n学生操作页面\n");
-        printf("1. 查看个人信息\n");
-        printf("2. 修改个人信息\n");
-        printf("0. 返回上一级\n");
-        printf("请选择操作：");
-        scanf("%d", &choice);
-
-        switch (choice) {
-            case 1:
-                display_student(student);
-                break;
-            case 2:
-                modify_student_info(student);
-                break;
-            case 0:
-                return;
-            default:
-                printf("无效的选择，请重新输入。\n");
-        }
-    }
-}
